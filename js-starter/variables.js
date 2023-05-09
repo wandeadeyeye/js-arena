02f129fb6ec41da11f19a583231bff0cc5ecb281
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-05-09 21:33:52 +0100

    variables-in-js

---
+++ js-starter/variables.js
@@ -0,0 +1,29 @@
+// VARIABLES IN JAVASCRPIT
+
+// var
+
+var number = 20;
+// console.log(number);
+// document.write(number)
+
+var number = 10;
+// console.log(number);
+
+number = 15;
+// console.log(number);
+
+// let
+
+let x = 20;
+// console.log(x);
+
+x = 10;
+// console.log(x);
+
+x = 12;
+// console.log(x);
+
+// const
+
+const y = 19;
+console.log(y);
