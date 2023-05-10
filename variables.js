df18e972a5153a41e99723364d0eb2e23cdc22d9
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-05-10 20:12:51 +0100

    variables-in-js

--- variables.js
+++ variables.js
@@ -1,30 +1,59 @@
-// VARIABLES IN JAVASCRPIT
+//////////////////  VARIABLES IN JAVASCRPIT   /////////////////////
 
-// VAR
+
+//// VAR
 
 var number = 20;
-// console.log(number);
-// document.write(number)
+console.log(number);
+document.write(number)
+
+//// Variable Mutation
 
 var number = 10;
-// console.log(number);
+console.log(number);
 
 number = 15;
-// console.log(number);
+console.log(number);
 
-// LET
+//// LET
 
 let x = 20;
-// console.log(x);
+console.log(x);
 
 x = 10;
-// console.log(x);
+console.log(x);
 
 x = 12;
-// console.log(x);
+console.log(x);
 
-// CONST
+//// CONST
 
 const y = 19;
 console.log(y);
 document.write(y);
+
+
+//// TYPE  COERCION
+
+
+var playerName = 'John';
+console.log(playerName);
+
+var playerNumber = 7;
+console.log(playerNumber);
+
+console.log(playerName + ' wears the number ' + playerNumber);
+
+var schoolName, hasCourse;
+
+schoolName = 'Meta Udemy';
+
+hasCourse = 'true';
+
+console.log(schoolName + '' + ' has 20 courses, is Data Sci. included? ' + hasCourse);
+// document.write(schoolName + '' + ' has 20 courses, is Data Sci. included? ' + hasCourse);
+
+alert(schoolName);
+
+var age = prompt('How old is he?')
+console.log('John is ' + age + ' years old' );
