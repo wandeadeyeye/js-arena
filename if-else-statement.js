91c3e81ba42c54e53753b7bbb69f5dd37ebb2fa5
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-05-14 14:31:14 +0100

    if-else-statement

---
+++ if-else-statement.js
@@ -0,0 +1,11 @@
+///////////   IF ELSE STATEMENT ////////////////
+
+var grade;
+
+grade = 69;
+
+if (grade >= 70) {
+    console.log('congratulation, you have been promoted!');
+} else {
+    console.log('sorry, you have to retake the class!');
+}
