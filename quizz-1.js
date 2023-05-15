a6483df772a2aed31135cb6632b310582c985592
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-05-15 20:59:11 +0100

    quizz-1

---
+++ quizz-1.js
@@ -0,0 +1,19 @@
+//////////////////  QUIZ 1  /////////////////////////
+
+var alexGrade, saraGrade, nancyGrade;
+
+alexGrade = (90 + 80 + 95) / 3;
+saraGrade = (89 + 76 + 98) / 3;
+nancyGrade = (42 + 98 + 83) / 3;
+
+console.log('Alex average grade is:', alexGrade);
+console.log('Sara average grade is:', saraGrade);
+console.log('Nancy average grade is:', nancyGrade);
+
+if (alexGrade > saraGrade && alexGrade > nancyGrade) {
+    console.log('Alex has the highest grade of them all', alexGrade);
+} else if (saraGrade <= alexGrade && saraGrade <= nancyGrade) {
+    console.log('Sara has the highest grade of them all', saraGrade);  
+} else {
+   console.log('Nancy has the highest grade of them all', nancyGrade); 
+}
