fff727962b0f85fcf7dbc7a2dc4d44b7bcf35cb5
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-05-18 21:13:55 +0100

    for-loop

--- for-loop.js
+++ for-loop.js
@@ -6,3 +6,10 @@ for (var number = 20; number <= 80; number++) {
     }
     
 }
+
+for (var a = 100, b =  200; a <= b; a++) {
+    if (a % 2 === 0 && a % 5 ===0) {
+        console.log(a);
+    }
+    a++;
+}
