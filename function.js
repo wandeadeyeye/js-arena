0ae0fef8dd655c4af42369fd6f5cf1d1bfaccb37
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-05-19 20:45:04 +0100

    function

---
+++ function.js
@@ -0,0 +1,17 @@
+////////////////////// FUNCTION //////////////////
+
+function sayHello() {
+    console.log('Hello, world!');
+}
+
+sayHello();
+
+function doSomething() {
+    console.log('Good Morning');
+    console.log('Good Afternoon');
+    console.log('Hello, you are welcome');
+}
+
+doSomething();
+doSomething();
+doSomething();
