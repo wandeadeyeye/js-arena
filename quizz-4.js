3599ecf9a4d3de77269a9ef7715b6b5aa4edf1fd
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-05-19 20:44:35 +0100

    quizz-4

---
+++ quizz-4.js
@@ -0,0 +1,34 @@
+/////////////// FUNCTION QUIZZ /////////////////
+
+///////////// 1
+
+function subtraction(x, y) {
+    return y - x;
+}
+
+var result = subtraction(4, 8);
+console.log(result);
+
+/////////////// 2
+
+function isOdd(number) {
+    if (number % 2 !== 0) {
+        return true;
+    }
+}
+
+var number = isOdd(7);
+console.log(number);
+
+/////////////// 3
+
+var addition = function (number) {
+    var jen = 0;
+    for (var i = 1; i <= number; i++) {
+        jen = jen + 1;
+    }
+    return jen;
+}
+
+var isAdd = addition(7);
+console.log(isAdd);
