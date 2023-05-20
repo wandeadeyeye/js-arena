55520ed1b9f80286c5f1a27aeff27a3963af63d2
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-05-20 21:14:48 +0100

    arrays

---
+++ array.js
@@ -0,0 +1,10 @@
+///////////////////// ARRAYS IN JS /////////////////////
+
+names = ['James', 'Tom', 'Jane','Jake']
+
+names[4] = 'Jason'
+console.log(names[4]);
+
+mainobj = [2000, 'John', 'jane', 'kia']
+
+console.log(mainobj);
