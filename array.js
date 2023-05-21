c4efe8c89cf46ac5d95bd0b5769f2a913424811b
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-05-21 19:10:51 +0100

    arrays

--- array.js
+++ array.js
@@ -1,10 +1,28 @@
 ///////////////////// ARRAYS IN JS /////////////////////
 
+var Name1 = 'James';
+var Name2 = 'Tom';
+
 names = ['James', 'Tom', 'Jane','Jake']
 
+console.log(names[2]);
+
+names[1] = 'Pepe'
+console.log(names);
+
 names[4] = 'Jason'
-console.log(names[4]);
+console.log(names);
 
-mainobj = [2000, 'John', 'jane', 'kia']
 
-console.log(mainobj);
+names[7] = 'Josh'
+console.log(names);
+
+var cat = [];
+var cat = new Array();
+
+var commonData = ['Jay', 25, true, null];
+console.log(commonData[1]);
+
+var cars = ['Jeep', 'Maybach','Porche','Volvo'];
+var car_lenth = cars.length;
+console.log(car_lenth);
