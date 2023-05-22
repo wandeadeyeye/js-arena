aaad3275dcf9cb4cec93ab7a32801e07a0eae6f9
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-05-22 19:01:23 +0100

    quizz-5

---
+++ quizz-5.js
@@ -0,0 +1,21 @@
+/////////////////// QUIZZ 5 ///////////////////////////
+
+/////////// 01
+
+var books = ['Harry Potter','Hobbit','Hungar Games','Little Prince'];
+
+var book = books[books.length - 1];
+console.log(book);
+
+
+
+//////////// 02
+
+var films = [
+    ['hobbit1','hobbit2','hobbit3'],
+    ['matrix1','matrix2','matrix3'],
+    ['godfather1','godfather2','godfather3']
+]
+
+var film = films[2][2]
+console.log(film);
