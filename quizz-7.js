0c53bdcb73028acd18f8ce64e7a1d638673079a2
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-05-26 22:21:38 +0100

    quizz-7

---
+++ quizz-7.js
@@ -0,0 +1,27 @@
+////////////////// QUIZZ - 7
+
+var universities = [
+    {
+        Name: 'MIT',
+        hasGone: true,
+        year: 1882
+    },
+    {
+        Name: 'Harvard',
+        hasGone: false,
+        year: 1739
+    },
+    {
+        Name: 'Stanford',
+        hasGone: true,
+        year: 1849
+    }
+];
+
+universities.forEach(function (university) {
+    if (university.hasGone === true) {
+        console.log(university.Name + university.year);
+    } else {
+        console.log(university.Name);
+    }
+});
