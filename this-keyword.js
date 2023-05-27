a3853d0ea08ed2e212de33815a2af012dc863422
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-05-27 16:41:21 +0100

    this-keyword

---
+++ this-keyword.js
@@ -0,0 +1,25 @@
+// console.log(this);
+
+function addition(x, y) {
+    console.log(x + y);
+    console.log(this);
+}
+
+addition(10, 20);
+
+var Raj = {
+    Name: 'Raj',
+    age: 21,
+    hasGone: true,
+    say: function () {
+        console.log(this);
+        console.log('say hi' + ' ' + this.Name);
+
+        function someFun() {
+            console.log(this);
+        }
+        someFun();
+    }
+};
+
+Raj.say();
