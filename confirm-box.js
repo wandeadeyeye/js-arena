24efeac0d324f310987ac91f6c0d71c27eaf5c3f
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-07-31 14:40:16 +0100

    Create confirm-box.js

---
+++ confirm-box.js
@@ -0,0 +1,11 @@
+////////////// CONFIRM BOX /////////////////
+
+// confirm('are you human?');
+
+var a = confirm('Are you Human?');
+
+if (a) {
+    alert('Thanks');
+} else {
+    alert('Its Okay');
+};
