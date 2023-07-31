d600ef7e04032a58b805ea9ef56f95b2e62aa154
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-07-31 14:40:19 +0100

    Create alert-boxes.js

---
+++ alert-boxes.js
@@ -0,0 +1,12 @@
+/////////////////////  ALERT BOXES  //////////////////////
+
+// alert('hello everyone')
+
+var a = 30;
+b = 10;
+
+if (a < b) {
+    alert('hello everyone')
+}else {
+    alert('hello john')
+};
