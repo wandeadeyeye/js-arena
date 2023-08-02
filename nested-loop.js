5ba3bab573d422988f1a86fbc9f83f4cd59150e6
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-08-02 20:32:04 +0100

    Create nested-loop.js

---
+++ nested-loop.js
@@ -0,0 +1,8 @@
+////////////////////////  NESTED LOOP  //////////////////////////
+
+for (var a = 3; a <= 100; a = a + 10) {
+    // console.log(a);
+    for (var b = a; b <= a + 10; b++) {
+        console.log(b);
+    }
+}
